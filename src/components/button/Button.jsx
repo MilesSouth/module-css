b76f440f482ceed2ad51de887a4c7ae6e8d4bc4a
SelimGüney <slmgny333@gmail.com>
2023-07-03 21:39:41 +0300

    button style module css usage

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,12 @@
 import ButonStyle from "./Button.module.css"
 
 const Button = ({ btnText }) => {
-  return <button>{btnText}</button>
+  return (
+    <>
+      <button className={ButonStyle["button-text"]}>{btnText}</button>
+      <button className={ButonStyle.button}>{btnText}</button>
+    </>
+  )
 }
 
 export default Button
